97732787fbde1c1f239048f4fdec050fc4babe76
ufqrf13vv <anarchy16@inbox.ru>
2016-11-10 12:51:49 +0600

    production

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,41 @@
 $(document).ready(function() {
+
+	//  Менюшка
 	$('.nav__burger').on('click', function () {
 		var nav = $('.nav__list');
 
 		nav.show('slow');
 	});
+
+	//  Вкладки
+	(function($){				
+	    jQuery.fn.lightTabs = function(options){
+	        
+	        var createTabs = function(){
+	            tabs = this;
+	            i = 0;
+	            
+	            showPage = function(i){
+	                $(tabs).children(".tabs__content").hide();
+	                $(tabs).children(".tabs__content").eq(i).show();
+	                $(tabs).children(".tabs__list").children(".tabs__item").removeClass("active");
+	                $(tabs).children(".tabs__list").children(".tabs__item").eq(i).addClass("active");
+	            }
+	            
+	            showPage(0);				
+	            
+	            $(tabs).children(".tabs__list").children(".tabs__item").each(function(index, element){
+	                $(element).attr("data-page", i);
+	                i++;                     
+	            });
+	            
+	            $(tabs).children(".tabs__list").children(".tabs__item").click(function(){
+	                showPage(parseInt($(this).attr("data-page")));
+	            });				
+	        };		
+	        return this.each(createTabs);
+	    };	
+	})(jQuery);
+
+	$(".tabs").lightTabs();
 });
